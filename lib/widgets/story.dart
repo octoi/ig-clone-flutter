78d9ee03a661d16973bc0444500c47de2e93c166
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-18 14:38:05 +0530

    displayed profile

---
+++ lib/widgets/story.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:ig_clone/utils/constants.dart';
+
+class Story extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+          padding: EdgeInsets.all(2.0),
+          decoration: BoxDecoration(
+            color: appPink,
+            shape: BoxShape.circle,
+          ),
+          child: CircleAvatar(
+            radius: 30.0,
+            backgroundImage: NetworkImage(
+              'https://cdn.pixabay.com/photo/2019/08/21/16/03/panda-4421395_960_720.jpg',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
