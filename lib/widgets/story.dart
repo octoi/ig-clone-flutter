5c1c0a7770cfa724ea3b0d30890553804a4649b1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-18 14:49:49 +0530

    add story btn

--- lib/widgets/story.dart
+++ lib/widgets/story.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:ig_clone/utils/constants.dart';
 
 class Story extends StatelessWidget {
+  final bool isMine;
+
+  const Story({
+    Key? key,
+    this.isMine = false,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,19 +17,45 @@ class Story extends StatelessWidget {
           margin: EdgeInsets.symmetric(horizontal: 5.0),
           padding: EdgeInsets.all(2.0),
           decoration: BoxDecoration(
-            color: appPink,
+            color: isMine ? null : appPink,
             shape: BoxShape.circle,
           ),
-          child: CircleAvatar(
-            radius: 30.0,
-            backgroundImage: NetworkImage(
-              'https://cdn.pixabay.com/photo/2019/08/21/16/03/panda-4421395_960_720.jpg',
-            ),
+          child: Stack(
+            children: [
+              CircleAvatar(
+                radius: 30.0,
+                backgroundImage: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2019/08/21/16/03/panda-4421395_960_720.jpg',
+                ),
+              ),
+              isMine
+                  ? Positioned(
+                      bottom: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        padding: EdgeInsets.all(0.5),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white30,
+                        ),
+                        child: CircleAvatar(
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: 20.0,
+                          ),
+                          backgroundColor: appPink,
+                          radius: 12.0,
+                        ),
+                      ),
+                    )
+                  : SizedBox(),
+            ],
           ),
         ),
         SizedBox(height: 5.0),
         Text(
-          "_panda_",
+          isMine ? 'Your Story' : "_panda_",
           style: TextStyle(
             color: appTextColor,
           ),
