b0d0da704b04d359717a9b1948b51faf2b42c9c3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-21 19:51:27 +0530

    added sample tab bar

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -8,44 +8,61 @@ class MessageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: Icon(Icons.arrow_back_ios),
-          color: appTextColor,
-        ),
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              getRandomUserName(),
-              style: TextStyle(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.arrow_back_ios),
+            color: appTextColor,
+          ),
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                getRandomUserName(),
+                style: TextStyle(
+                  color: appTextColor,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.keyboard_arrow_down),
                 color: appTextColor,
               ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.videocam),
+              color: appTextColor,
+              iconSize: 35.0,
             ),
             IconButton(
               onPressed: () {},
-              icon: Icon(Icons.keyboard_arrow_down),
+              icon: Icon(Icons.edit_outlined),
               color: appTextColor,
             ),
           ],
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Text('Primary', style: TextStyle(color: appTextColor)),
+              ),
+              Tab(
+                child: Text('General', style: TextStyle(color: appTextColor)),
+              ),
+            ],
+          ),
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(CupertinoIcons.videocam),
-            color: appTextColor,
-            iconSize: 35.0,
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.edit_outlined),
-            color: appTextColor,
-          ),
-        ],
+        body: TabBarView(children: [
+          Icon(Icons.directions_car),
+          Icon(Icons.directions_transit),
+        ]),
       ),
     );
   }
