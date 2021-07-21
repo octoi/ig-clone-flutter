95dd8530fc8b0de951fc8b03a73ab2eb80dc7b77
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-21 19:34:31 +0530

    added arrow back icon in app bar

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:ig_clone/utils/constants.dart';
 
 class MessageScreen extends StatelessWidget {
   const MessageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_ios),
+          color: appTextColor,
+        ),
+      ),
+    );
   }
 }
