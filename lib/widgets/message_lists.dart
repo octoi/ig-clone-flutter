add48a9dd35527c9ac23096363f640e63e993cec
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-21 20:08:02 +0530

    displayed camera

--- lib/widgets/message_lists.dart
+++ lib/widgets/message_lists.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ig_clone/utils/constants.dart';
 import 'package:ig_clone/utils/data.dart';
@@ -30,36 +31,64 @@ class MessageList extends StatelessWidget {
               prefixIcon: Icon(Icons.search),
             ),
           ),
-          Container(
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(getRandomImage()),
-                  radius: 28.0,
-                ),
-                SizedBox(width: 20.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      getRandomUserName(),
-                      style: TextStyle(
-                        color: appTextColor,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
+          InkWell(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(getRandomImage()),
+                        radius: 28.0,
                       ),
-                    ),
-                    SizedBox(height: 5.0),
-                    Text(
-                      "Hello there",
-                      style: TextStyle(
-                        color: appTextColor,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                      SizedBox(width: 20.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            getRandomUserName(),
+                            style: TextStyle(
+                              color: appTextColor,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Row(
+                            children: [
+                              Text(
+                                "Hello there",
+                                style: TextStyle(
+                                  color: appTextColor,
+                                  fontSize: 15.0,
+                                ),
+                              ),
+                              Text(
+                                " • 5h",
+                                style: TextStyle(
+                                  color: appTextColor.withOpacity(0.5),
+                                  fontSize: 15.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.camera_alt_outlined),
+                    color: appTextColor.withOpacity(0.7),
+                    iconSize: 25.0,
+                  )
+                ],
+              ),
             ),
           ),
         ],
