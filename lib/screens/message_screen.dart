96a62ff398a40c54938bd058ad7fe0a16729dd18
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-21 19:38:52 +0530

    title

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ig_clone/utils/constants.dart';
+import 'package:ig_clone/utils/data.dart';
 
 class MessageScreen extends StatelessWidget {
   const MessageScreen({Key? key}) : super(key: key);
@@ -14,6 +16,22 @@ class MessageScreen extends StatelessWidget {
           onPressed: () => Navigator.pop(context),
           icon: Icon(Icons.arrow_back_ios),
           color: appTextColor,
+        ),
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              getRandomUserName(),
+              style: TextStyle(
+                color: appTextColor,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.keyboard_arrow_down),
+              color: appTextColor,
+            ),
+          ],
         ),
       ),
     );
